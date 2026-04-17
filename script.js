643f332c058d9fdcd7e7e14bb4a88b1bdcd8f90c
patrick <j.patrickagravante@gmail.com>
2026-04-17 21:14:51 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+//Get the Element ID's
+const ValidationForm = {
+     validForm: document.getElementById("validForm"),
+     form:document.getElementById("form"),
+     missionName: document.getElementById("missionName"),
+     commanderName: document.getElementById("commanderName"),
+     status: document.getElementById("status")
+}
+
+const DashboardContainer = {
+     dashboardContainer: document.getElementById("mainDashboard")
+}
+
+//functions
+function dashboard(){
+     ValidationForm.validForm.classList.add("hide")
+
+     DashboardContainer.dashboardContainer.classList.add("show")
+}
+
+function getInputs(){
+     return {
+          missionName: ValidationForm.missionName.value.trim(),
+          commanderName: ValidationForm.commanderName.value.trim()
+     }
+}
+
+function logInStatus(){
+     ValidationForm.status.innerHTML = '<p style="color: gray;">Verifying...</p>';
+        
+          setTimeout(()=>{
+               ValidationForm.status.innerHTML = '<p style="color: green;"><i class="fas fa-check-circle"> </i> Verify Complete </i></p>';
+               setTimeout(()=>{
+                    ValidationForm.status.innerHTML = '<p style="color: gray;">Redirecting</p>';
+               }, 2000)       
+          }, 1000)
+}
+
+ValidationForm.form.addEventListener("submit", (e)=>{
+     e.preventDefault()
+     const {missionName, commanderName} = getInputs();   
+     if (missionName === "" || commanderName === ""){
+          ValidationForm.status.innerHTML = '<p style="color: red;">Input Required</p>';
+     } else{
+          logInStatus(); 
+          dashboard();
+     }
+})
